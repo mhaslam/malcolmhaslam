2e715613e32ef870c29fbcd9a7976f2d65dd921f
Malcolm HASLAM <mhspecialitycoffee@gmail.com>
2024-02-23 10:32:28 +0100

    fixed star separator, buttons on small screens and joke size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,21 @@ import { RouterOutlet } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TaskbarComponent } from './taskbar/taskbar.component';
 import { PopWindowsComponent } from './pop-windows/pop-windows.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FlexLayoutModule,TaskbarComponent,PopWindowsComponent],
+  imports: [CommonModule, RouterOutlet, FlexLayoutModule,TaskbarComponent,PopWindowsComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'MalcolmHaslam';
+    //icons
+    icons = {
+      star: faStar
+    }
 }
